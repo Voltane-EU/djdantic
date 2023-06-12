ffee89acea1347c1613debdd3f81dd67b5c6674e
Manuel Stingl <manuel@stingl.st>
2023-06-12 09:40:22 +0200

    add typing util for many to many fields

--- djdantic/__init__.py
+++ djdantic/__init__.py
@@ -1,4 +1,5 @@
 from .models import BaseModel, ModelKind
 from .fields import Field
 from .utils.pydantic_django import transfer_from_orm, transfer_to_orm
+from .utils.pydantic import Reference
 from pydantic.fields import Undefined
--- djdantic/utils/typing.py
+++ djdantic/utils/typing.py
@@ -1,6 +1,12 @@
-from typing import Type, TYPE_CHECKING, TypeVar
+from typing import Type, TYPE_CHECKING, TypeVar, Generic
+from django.db import models
+
+if TYPE_CHECKING:
+    from django.db.models.manager import ManyToManyRelatedManager
+
 
 T = TypeVar('T')
+M = TypeVar('M', bound=models.Model)
 
 
 def with_typehint(baseclass: Type[T]) -> Type[T]:
@@ -16,3 +22,13 @@ def with_typehint(baseclass: Type[T]) -> Type[T]:
         return baseclass
 
     return object  # noqa
+
+
+if not TYPE_CHECKING:
+    class ManyToManyRelatedManager(Generic[M]):
+        """
+        Pydantic Compatible Generic Type for declaring a ManyToManyField
+        """
+        @classmethod
+        def __get_validators__(cls):
+            yield lambda v: v
