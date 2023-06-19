99b23e755ccc185312e36e8b658c777027b564ec
Manuel Stingl <manuel@stingl.st>
2023-06-19 13:32:07 +0200

    add method to get field type

--- djdantic/utils/typing.py
+++ djdantic/utils/typing.py
@@ -1,4 +1,7 @@
 from typing import Type, TYPE_CHECKING, TypeVar, Generic
+from decimal import Decimal
+from uuid import UUID
+from datetime import date, datetime, timedelta, time
 from django.db import models
 
 if TYPE_CHECKING:
@@ -7,6 +10,36 @@ if TYPE_CHECKING:
 
 T = TypeVar('T')
 M = TypeVar('M', bound=models.Model)
+
+
+FIELD_TYPE = {
+    models.AutoField: int,
+    models.BigAutoField: int,
+    models.BigIntegerField: int,
+    models.BinaryField: bytes,
+    models.BooleanField: bool,
+    models.CharField: str,
+    models.DateField: date,
+    models.DateTimeField: datetime,
+    models.DecimalField: Decimal,
+    models.DurationField: timedelta,
+    models.EmailField: str,
+    models.FilePathField: str,
+    models.FloatField: float,
+    models.GenericIPAddressField: str,
+    models.IPAddressField: str,
+    models.IntegerField: int,
+    models.PositiveBigIntegerField: int,
+    models.PositiveIntegerField: int,
+    models.PositiveSmallIntegerField: int,
+    models.SlugField: str,
+    models.SmallAutoField: int,
+    models.SmallIntegerField: int,
+    models.TextField: str,
+    models.TimeField: time,
+    models.URLField: str,
+    models.UUIDField: UUID,
+}
 
 
 def with_typehint(baseclass: Type[T]) -> Type[T]:
@@ -32,3 +65,10 @@ if not TYPE_CHECKING:
         @classmethod
         def __get_validators__(cls):
             yield lambda v: v
+
+
+def get_field_type(field: models.Field):
+    if isinstance(field, models.ForeignKey):
+        field.model.__annotations__.get(field.name, field.related_model)
+
+    return field.model.__annotations__.get(field.name, FIELD_TYPE.get(type(field)))
